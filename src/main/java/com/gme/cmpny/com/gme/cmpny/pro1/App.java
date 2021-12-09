ef0a772a59562d684414940e0c303206fdb976fe
ChAnDu <ChAnDu@DESKTOP-7RSL3B7>
2021-12-09 22:48:54 +0530

    push

---
+++ src/main/java/com/gme/cmpny/com/gme/cmpny/pro1/App.java
@@ -0,0 +1,13 @@
+package com.gme.cmpny.com.gme.cmpny.pro1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
